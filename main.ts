bc4bf75b01961bcdd822f6219b343854b45e7bde
Matthew Coleman <matthewdecoleman@gmail.com>
2024-09-28 22:26:45 +0100

    added: basic route handler

--- main.ts
+++ main.ts
@@ -1,7 +1,14 @@
 import { Server } from "./server.ts";
 
 function start() {
-  const server = new Server().port(3000);
+  const server = new Server()
+    .port(3000)
+    .get("/", async (_req, res) => {
+      res.setBody("Application root").setStatusCode(200);
+    })
+    .get("/transactions", async (_req, res) => {
+      res.setBody("Transactions root").setStatusCode(200);
+    });
 
   server.listen();
 }
--- server.ts
+++ server.ts
@@ -1,92 +1,97 @@
-import { RequestBuilder } from "./request-builder.ts";
+import { RequestBuilder, Request } from "./request-builder.ts";
 import { ResponseBuilder } from "./response-builder.ts";
 
 async function writeResponse(connection: Deno.Conn, response: string) {
   await connection.write(new TextEncoder().encode(response));
 }
 
-async function connectionHandler(connection: Deno.Conn) {
+async function parseRequest(connection: Deno.Conn) {
   const buffer = new Uint8Array(1024);
 
   const bytesRead = await connection.read(buffer);
 
-  if (bytesRead === null) {
-    const response = new ResponseBuilder().setStatusCode(400).build();
+  if (bytesRead === null) throw Error("TODO: handle null request");
 
-    await writeResponse(connection, response);
-  } else {
-    const rawRequest = new TextDecoder().decode(buffer.subarray(0, bytesRead));
+  const rawRequest = new TextDecoder().decode(buffer.subarray(0, bytesRead));
 
-    const requestBuilder = new RequestBuilder();
+  const requestBuilder = new RequestBuilder();
 
-    const requestParts = rawRequest.split("\r\n");
+  const requestParts = rawRequest.split("\r\n");
 
-    let isEndOfHeader = false;
+  let isEndOfHeader = false;
 
-    for (let partIndex = 0; partIndex < requestParts.length; partIndex++) {
-      const part = requestParts[partIndex];
+  for (let partIndex = 0; partIndex < requestParts.length; partIndex++) {
+    const part = requestParts[partIndex];
 
-      // Request line
-      if (partIndex === 0) {
-        const reqestLine = part.split(" ");
+    // Request line
+    if (partIndex === 0) {
+      const reqestLine = part.split(" ");
 
-        requestBuilder.setMethod(reqestLine[0]);
-        requestBuilder.setPath(reqestLine[1]);
-        requestBuilder.setHttpVersion(reqestLine[2]);
+      requestBuilder.setMethod(reqestLine[0]);
+      requestBuilder.setPath(reqestLine[1]);
+      requestBuilder.setHttpVersion(reqestLine[2]);
 
-        continue;
-      }
-
-      // Add Header
-      if (!isEndOfHeader && part.length > 0) {
-        const [key, value] = part.split(":");
-
-        requestBuilder.addHeader(key.trim(), value.trim());
-      }
-
-      // End of Headers
-      if (!isEndOfHeader && part === "") {
-        isEndOfHeader = true;
-        continue;
-      }
-
-      if (isEndOfHeader) {
-        if (part && part.length > 0) {
-          console.info("body", part);
-          // Set Body
-          requestBuilder.setBody(part);
-          break;
-        }
-      }
+      continue;
     }
 
-    const request = requestBuilder.build();
+    // Add Header
+    if (!isEndOfHeader && part.length > 0) {
+      const [key, value] = part.split(":");
 
-    const response = new ResponseBuilder()
-      .setStatusCode(200)
-      .addHeader("Content-Type", "application/json")
-      .setBody(
-        JSON.stringify(
-          {
-            ...request,
-            headers: Object.fromEntries(request.headers),
-          },
-          (_key, value) => (value === null ? undefined : value)
-        )
-      )
-      .build();
+      requestBuilder.addHeader(key.trim(), value.trim());
+    }
 
-    await writeResponse(connection, response);
+    // End of Headers
+    if (!isEndOfHeader && part === "") {
+      isEndOfHeader = true;
+      continue;
+    }
+
+    if (isEndOfHeader) {
+      if (part && part.length > 0) {
+        console.info("body", part);
+        // Set Body
+        requestBuilder.setBody(part);
+        break;
+      }
+    }
   }
 
-  return connection.close();
+  const request = requestBuilder.build();
+
+  return request;
+}
+
+interface RouteHandler {
+  method: "GET" | "POST";
+  path: string;
+  handler: (req: Request, res: ResponseBuilder) => Promise<void>;
 }
 
 export class Server {
   private _port = 8080;
+  private _routeHandlers: RouteHandler[] = [];
 
   port(port: number) {
     this._port = port;
+    return this;
+  }
+
+  get(
+    path: string,
+    handler: (req: Request, res: ResponseBuilder) => Promise<void>
+  ) {
+    this._routeHandlers.push({ method: "GET", path, handler });
+
+    return this;
+  }
+
+  post(
+    path: string,
+    handler: (req: Request, res: ResponseBuilder) => Promise<void>
+  ) {
+    this._routeHandlers.push({ method: "POST", path, handler });
+
     return this;
   }
 
@@ -96,7 +101,22 @@ export class Server {
     console.info(`Server listening on port ${this._port}`);
 
     for await (const connection of listener) {
-      connectionHandler(connection);
+      const request = await parseRequest(connection);
+      const response = new ResponseBuilder();
+
+      for (let i = 0; i < this._routeHandlers.length; i++) {
+        const { handler, method, path } = this._routeHandlers[i];
+
+        console.info("handler path", path);
+        console.info("request path", request.path);
+        if (method === request.method && path === request.path) {
+          await handler(request, response);
+          break;
+        }
+      }
+
+      await writeResponse(connection, response.build());
+      connection.close();
     }
   }
 }
